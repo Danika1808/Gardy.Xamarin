74eb57bd53b7db6a9e9e872b1fdc5ef448c03abb
Danika1808 <65758418+Danika1808@users.noreply.github.com>
2022-06-17 12:13:47 +0300

    Initial commit

---
+++ Gardy/Gardy/Gardy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Gardy.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Gardy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
